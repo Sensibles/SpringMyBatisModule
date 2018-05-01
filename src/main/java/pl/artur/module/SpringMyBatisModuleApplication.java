c8268833b18a6d17ff477c5dc4f6e738c107743f
Artur Laptop <sensibles93@gmail.com>
2018-05-01 10:47:28 +0200

    Initial commit

---
+++ src/main/java/pl/artur/module/SpringMyBatisModuleApplication.java
@@ -0,0 +1,12 @@
+package pl.artur.module;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMyBatisModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMyBatisModuleApplication.class, args);
+	}
+}
