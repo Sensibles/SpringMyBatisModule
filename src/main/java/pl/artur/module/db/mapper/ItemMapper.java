527370e4a70d89568894274ba253f44505dc5445
Artur Laptop <sensibles93@gmail.com>
2018-05-01 14:03:56 +0200

    pom.xml finally working

--- src/main/java/pl/artur/module/db/mapper/ItemMapper.java
+++ src/main/java/pl/artur/module/db/mapper/ItemMapper.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ItemMapper {
 	
-	@Insert("INSERT INTO ITEMS (ID, ITEMNAME, AMOUNT) values(#{id}, #{itemName}, #{amount}")
+	@Insert("INSERT INTO ITEMS (ID, ITEMNAME, AMOUNT) values(#{id}, #{itemName}, #{amount})")
 	public void insertItem(@Param(value="id")long id, @Param(value="itemName")String itemName, @Param(value="amount")int amount);
 
 }
